b7c66aa9cacf62cd04c1d5cf974bb4591d9f8649
Carsten Bormann <cabo@tzi.org>
2023-08-26 20:59:30 +0200

    Add b64 grammar (but still process manually)

--- lib/cbor-diagnostic-app/b64.rb
+++ lib/cbor-diagnostic-app/b64.rb
@@ -1,7 +1,13 @@
-# mockup only; needs parser!
+require_relative "b64grammar.rb"
 
 class CBOR_DIAG::App_b64
   def self.decode(_, s)
+    parser = B64GRAMMARParser.new #  B? XXX
+    ast = parser.parse(s)
+    if !ast
+      raise ArgumentError, "cbor-diagnostic: Parse Error in b64'#{s}':\n" << EDN.reason(parser, s)
+    end
+    # lazy -- not using parse tree...:
     t = s.gsub(/\s/, '').chars.each_slice(4).map(&:join)
     if last = t[-1]
       last << "=" * (4 - last.size)
