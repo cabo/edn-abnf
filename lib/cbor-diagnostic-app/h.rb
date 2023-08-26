88e58694dae34c7bf662384bd39d5372c7711a82
Carsten Bormann <cabo@tzi.org>
2023-08-26 20:57:51 +0200

    Small fix to diagnostic

--- lib/cbor-diagnostic-app/h.rb
+++ lib/cbor-diagnostic-app/h.rb
@@ -5,10 +5,9 @@ class CBOR_DIAG::App_h
     parser = HGRAMMARParser.new
     ast = parser.parse(s)
     if !ast
-      raise ArgumentError, "Parse Error in h'#{s}':\n" << EDN.reason(parser, s)
+      raise ArgumentError, "cbor-diagnostic: Parse Error in h'#{s}':\n" << EDN.reason(parser, s)
     end
     ast.ast
   end
-
-#    s.gsub(/#.*|\s|\/[^\/]*\//, "").chars.each_slice(2).map{ |x| Integer(x.join, 16).chr("BINARY") }.join
+# s.gsub(/#.*|\s|\/[^\/]*\//, "").chars.each_slice(2).map{ |x| Integer(x.join, 16).chr("BINARY") }.join
 end
