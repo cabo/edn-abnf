e6f78088ed77128482c66c95f06281b3cd079a84
Carsten Bormann <cabo@tzi.org>
2026-04-20 19:52:20 +0200

    Refactor test generator for simple.csv

--- tests/simple.rb
+++ tests/simple.rb
@@ -1,4 +1,5 @@
 require 'cbor-diagnostic'
+require 'csv'
 class String
   def hexi
     bytes.map{|x| "%02x" % x}.join
@@ -6,12 +7,20 @@ class String
 end
 
 Simple_OK = Set.new(0..23) + Set.new(32..255)
-[nil, "foo".b, "bar".b, *(-10..300)].each do |num|
-  if Simple_OK.include? num
-    s = CBOR::Simple.new(num)
-    puts "x,#{s.inspect},#{s.to_cbor.hexi}"
-  else
-    d = num.cbor_diagnostic
-    puts "-,simple(#{d}),simple(#{d})"
+
+out = CSV.generate do |csv|
+  [nil, "foo", "bar".b, *(-10..300)].each do |num|
+    if Simple_OK.include? num
+      s = CBOR::Simple.new(num)
+      csv << ["x", s.inspect, s.to_cbor.hexi]
+    else
+      d = num.cbor_diagnostic
+      s = "simple(#{d})"
+      csv << ["-", s, s]
+    end
   end
 end
+
+puts out
+
+
